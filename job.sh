f4dbc42871ad36f31f001a386ad8d38ac8f005e7
Овчар Евгений УРФУ <92199120+eleyterokokus@users.noreply.github.com>
2024-06-10 06:44:05 +1100

    Add files via upload

---
+++ job.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+python3 ./mlops-demo/data_creation.py
+python3 ./mlops-demo/model_preprocessing.py
+python3 ./mlops-demo/model_preparation.py
+python3 ./mlops-demo/model_testing.py
+
+# optional
+python3 ./mlops-demo/cleaner.py
